6f6ea3430a5d14a7a05be3069856b8b87f213d18
Dorothy Mae Berana <dorberana@my.cspc.edu.ph>
2023-02-21 11:13:47 +0800

    update

---
+++ welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE.html>
+<html lang"en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equi="X-UA-Compatible" content="IE=edge">
+    <meta name="viewort" content="width=device-width, initial-scale=1.0">
+    <title>Berana</title>
+</head>
+<body>
+    <h1>My very First Laravel Application</h1>
+    <h1>Welcome to my First Laravel Application</h1>
+</body>
+</html>
